f6390acfece8b3e3b2ff6bbbff2b63507e059fc8
Akio Kondo <kojiran@gmail.com>
2023-07-07 04:45:26 +0900

    人数更新用

--- aggregater.py
+++ aggregater.py
@@ -7,6 +7,7 @@ from datetime import datetime
 import json
 from tqdm import tqdm
 
+# connection = sqlite3.connect("atp_sandbox.db")
 connection = sqlite3.connect("atp.db")
 cur = connection.cursor()
 
@@ -29,9 +30,10 @@ load_dotenv(dotenv_path)
 def get_did_list(after=None):
   prev = datetime.utcnow()
   url = 'https://plc.directory/export'
+  # url = 'https://plc.bsky-sandbox.dev/export'
   if after:
     url += f"?after={after}"
-  response = requests.get(url)
+  response = requests.get(url, timeout=(15, 15))
   now = datetime.utcnow()
   print(now - prev)
   return response.text
@@ -47,7 +49,21 @@ def insert_did_many(connection, did_list):
   connection.commit()
 
 
-last_created_at = None
+def get_last_created_at(connection):
+  cur = connection.cursor()
+  sql = """
+    SELECT *
+    FROM users
+    ORDER BY created_at DESC
+    LIMIT 1
+  """
+  cur = connection.cursor()
+  cur.execute(sql)
+  row = cur.fetchone()
+  return row[4]
+
+
+last_created_at = get_last_created_at(connection)
 
 while True:
   did_list_text = get_did_list(last_created_at)
@@ -63,7 +79,7 @@ while True:
       did_list.append((did_dict["did"].replace("did:plc:", ""),
                       did_dict["operation"]["handle"],
                       endpoint,
-                      did_dict["createdAt"]))
+                      did_dict["createdAt"].replace('T', ' ').replace('Z', '')))
     elif did_dict["operation"]["type"] == "plc_operation":
       if did_dict["operation"]["prev"] is None and \
               "atproto_pds" in did_dict["operation"]["services"]:
@@ -72,7 +88,7 @@ while True:
         did_list.append((did_dict["did"].replace("did:plc:", ""),
                         handle,
                         endpoint,
-                        did_dict["createdAt"]))
+                        did_dict["createdAt"].replace('T', ' ').replace('Z', '')))
   last_created_at = did_dict["createdAt"]
   if last_created_at == last_created_at_prev:
     break
